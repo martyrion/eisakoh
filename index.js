678a15ca2ba89194169939d5b12e80f41a23848c
martyrion <63229987+martyrion@users.noreply.github.com>
2022-09-29 11:32:06 +0300

    Further updates

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ window.addEventListener("resize", () => {
     document.body.classList.remove("resize-animation-stopper");
   }, 1000);
 });
+
+document.querySelectorAll(".parallax").forEach((elem) => {
+  const modifier = elem.getAttribute("data-modifier");
+
+  basicScroll
+    .create({
+      elem: elem,
+      from: 0,
+      to: 519,
+      direct: true,
+      props: {
+        "--translateY": {
+          from: "0",
+          to: `${10 * modifier}px`,
+        },
+      },
+    })
+    .start();
+});
